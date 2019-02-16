c24e096b6232d5ee6f39b157e6ec703c8143d2b6
Jingyun Yang <jingyuny@usc.edu>
2019-02-16 22:46:44 +0000

    Added demo data, information retrieval API's, and image cropping functionalities.

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const app = express()
 const fs = require('fs')
 const multer = require('multer')
 const posenetUtils = require('./libs/posenet_utils')
+const path = require('path')
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.send('Hello World!')
+})
+
+app.get('/catalog/:category', function (req, res) {
+    var catalog = JSON.parse(fs.readFileSync('data/catalog.json', 'utf8'))
+    res.send(JSON.stringify(catalog[req.params.category]))
+})
+
+app.get('/exercise/video/:id', function (req, res) {
+    res.sendFile(path.join(__dirname, 'data/demos/' + req.params.id + '.mp4'))
+})
+
+app.get('/exercise/pose/:id', function (req, res) {
+    var poses = JSON.parse(fs.readFileSync('data/poses.json', 'utf8'))
+    res.send(JSON.stringify(poses[req.params.id]))
 })
 
 const upload = multer({ dest: 'uploads/pose/' })
@@ -16,7 +30,10 @@ app.post('/pose', upload.single('image'), function (req, res) {
     posenetUtils.predict(req.file.path).then(function (pose) {
         pose['input_filename'] = req.file.originalname
         pose['keypoints'] = pose['keypoints'].reduce(function (dict, item) {
-            dict[item['part']] = [item['position']['x'], item['position']['y']]
+            dict[item['part']] = [
+                item['position']['x'] / posenetUtils.inputImageSize, 
+                item['position']['y'] / posenetUtils.inputImageSize
+            ]
             return dict
         }, {})
         delete pose['score']
--- libs/posenet_utils.js
+++ libs/posenet_utils.js
@@ -3,13 +3,39 @@ const posenet = require('@tensorflow-models/posenet')
 const sizeOf = require('image-size')
 const { createCanvas, loadImage } = require('canvas')
 const sleep = require('system-sleep')
+const fs = require('fs')
+
+// Preparations: load the network
 
 var net = null
 posenet.load().then(function (model) {
     net = model
 })
 
+// Helper functions
+
+function getCropCoords (width, height) {
+    let source_center_x = width / 2
+    let source_center_y = height / 2
+
+    let source_side_length = Math.min(width, height)
+    let source_x = source_center_x - source_side_length / 2
+    let source_y = source_center_y - source_side_length / 2
+
+    return {
+        "sx": source_x,
+        "sy": source_y,
+        "sw": source_side_length,
+        "sh": source_side_length
+    }
+}
+
+// Exported methods
+
+const inputImageSize = 200
+
 module.exports = {
+    inputImageSize: inputImageSize,
     predict: async function (path) {
         const imageScaleFactor = 0.5
         const flipHorizontal = false
@@ -18,11 +44,15 @@ module.exports = {
         const size = sizeOf(path)
         const { width, height } = size
 
-        const canvas = createCanvas(width, height)
+        const cropCoords = getCropCoords(width, height)
+
+        const canvas = createCanvas(cropCoords['sw'], cropCoords['sh'])
         const ctx = canvas.getContext('2d')
         
         const image = await loadImage(path)
-        ctx.drawImage(image, 0, 0, width, height)
+        ctx.drawImage(image, cropCoords['sx'], cropCoords['sy'],
+                      cropCoords['sw'], cropCoords['sh'],
+                      0, 0, inputImageSize, inputImageSize)
 
         while(net == null) {
             sleep(100)
